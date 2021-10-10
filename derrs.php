a7234f1fbd133836c8f9beca8cfb637a66088921
yitekchi <91290365+yitekchi@users.noreply.github.com>
2021-10-10 22:29:57 +0300

    Add files via upload

---
+++ derrs.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<!-- Fontfaces CSS-->
+	<link href="../assist/css/font-face.css" rel="stylesheet" media="all">
+	<link href="../vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+	<!-- Bootstrap CSS-->
+	<link href="../vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+	<!-- Vendor CSS-->
+	<link href="../vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/wow/animate.css" rel="stylesheet" media="all">
+	<link href="../vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/slick/slick.css" rel="stylesheet" media="all">
+	<link href="../vendor/select2/select2.min.css" rel="stylesheet" media="all">
+	<link href="../vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+	<!-- Main CSS-->
+	<link href="../assist/css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+
+<style>
+	
+	* {
+		margin: 0;
+		padding: 0;
+	  
+	}
+	*, ::after, ::before {
+		box-sizing: border-box;
+	}
+	
+	body {
+		font-family: "Poppins", sans-serif;
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 1.625;
+		color: #666;
+	   background-color:black;
+	}
+		body {
+		margin: 0;
+		font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+		font-size: 1rem;
+		font-weight: 600;
+		line-height: 2;
+		color: #212529;
+		text-align: left;
+		background-color:#e5e5e5;
+	
+	
+	-smoothing: grayscale;
+	}
+	html {
+		font-family: sans-serif;
+		line-height: 1.15;
+		
+	   }
+	*, ::after, ::before {
+		box-sizing: border-box;
+	}
+	*, ::after, ::before {
+		box-sizing: border-box;
+	}
+	
+
+	.navbar-sidebar{
+	   color:white;
+	   background-color:white;
+	   width: 16rem;
+	   margin-right:60px;
+	 
+	}
+	
+	
+/*men yazgan ye*/
+.yer{
+	
+	width: 100%;
+	
+}
+.orun{
+	display:flex;
+	width: 99%;
+	height: 700px;
+
+	margin: auto;
+	padding: 2em;
+
+}
+.sidebar{
+	width: 20%;
+	height: 100%;
+padding-right: 0.50rem;
+
+}
+.ders{
+	width: 30%;
+	height: 100%;
+
+}
+.news{
+	width: 50%;
+	height: 100%;
+	
+}
+	
+	.btn-thumbs-up,.btn-thumbs-down,.btn-comments,.btn-chevron-down{
+		color:grey;
+	}
+	  
+	
+	.col-lg-6.kkk,.col-lg-6.zzz {
+		max-width: 100%;
+		height: 100%;
+	
+	}
+	.navbar-sidebar{
+		width: 100%;
+	}
+	.col-lg-6.zzz{
+		width: 100%;
+		height: 100%;
+	
+	}
+	.mm{
+		height: 100%;
+	}
+.au-card{
+	height: 100%;
+}
+.navbar-sidebar{
+	height: 100%;
+}
+.icon{
+	display:flex;
+
+}
+.time{
+	margin-right:15px;
+}
+.buttonn{
+	margin-left:auto;
+}
+
+.button{
+	border-radius:5px;
+	background-color:#d8d5d3;
+	width: 4rem;
+
+}
+	
+	
+	
+	
+	</style>
+
+<body>
+	<div class="yer">
+		<div class="orun">
+<!--saidebar baxlandi-->
+			<div class="sidebar">
+			<div class="mm">
+					<nav class="navbar-sidebar">
+						<ul class="list-unstyled navbar__list">
+							<li class="has-sub">
+								<a class="js-arrow" href="#">
+									<i class="fas fa-tachometer-alt"></i>Dashboard</a>
+								
+							</li>
+							<li>
+								<a href="user.php">
+									<i class="fas fa-bold"></i>users</a>
+							</li>
+						
+							<li>
+								<a href="table.php">
+									<i class="fas fa-table"></i>Tables</a>
+							</li>
+							<li>
+								<a href="form.php">
+									<i class="far fa-check-square"></i>Forms</a>
+							</li>
+							<li>
+								<a href="calendar.php">
+									<i class="fas fa-calendar-alt"></i>Calendar</a>
+							</li>
+							<li>
+								<a href="map.php">
+									<i class="fas fa-map-marker-alt"></i>Maps</a>
+							</li>
+						
+						</ul>
+					</nav>
+			   </div>
+
+			</div>
+<!--saidebar ahirlaxti-->
+<!--news baxlandi -->
+<div class="news">
+			<div class="col-lg-6 kkk">
+					<div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
+						<div class="au-card-title" style="background-image:url('images/bg-title-01.jpg');">
+							<div class="bg-overlay bg-overlay--blue"></div>
+							<h3>
+								<i class="zmdi zmdi-account-calendar"></i>26 April, 2018</h3>
+							<button class="au-btn-plus">
+								<i class="zmdi zmdi-plus"></i>
+							</button>
+						</div>
+						<div class="au-task js-list-load">
+							<div class="au-task__title">
+								<p>bugunki yegi hewerler</p>
+							</div>
+							
+								<div class="au-task__item au-task__item--danger">
+									<div class="au-task__item-inner">
+										<p>qikagan kixi ismi </p>
+										<h5 class="task">
+										<a href="#"> soralgan sual:Meeting about plan for Admin Template 2018</a>
+										<p>jawap: soralgan sual:Meeting about plan for Admin Template 2018 soralgan sual:Meeting about plan for Admin Template 2018</p>
+									    </h5>
+
+										<div class="icon">
+										<span class="time">10:00 AM</span>
+										<ul class="list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-up"> <i class="far fa-thumbs-up"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-down"><i class="far fa-thumbs-down"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-comments"><i class="far fa-comments"></i></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-chevron-down"><i class="fas fa-chevron-down"></i></a>
+								</li>
+							</ul>
+
+							<div class="buttonn">
+							<button class="button">js</button> 
+							<button class="button">js</button>
+							</div>
+
+										</div>
+
+									</div>
+								</div>
+								<div class="au-task__item au-task__item--warning">
+									<div class="au-task__item-inner">
+										<h5 class="task">
+											<a href="#">Create new task for Dashboard</a>
+										</h5>
+
+										<div class="icon">
+										<span class="time">11:00 AM</span>
+										<ul class="list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-up"> <i class="far fa-thumbs-up"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-down"><i class="far fa-thumbs-down"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-comments"><i class="far fa-comments"></i></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-chevron-down"><i class="fas fa-chevron-down"></i></a>
+								</li>
+							</ul>
+										</div>
+
+									
+									</div>
+								</div>
+								<div class="au-task__item au-task__item--primary">
+									<div class="au-task__item-inner">
+									<p>qikagan kixi ismi </p>
+										<h5 class="task">
+										<a href="#"> soralgan sual:Meeting about plan for Admin Template 2018</a>
+										<p>jawap: soralgan sual:Meeting about plan for Admin Template 2018 soralgan sual:Meeting about plan for Admin Template 2018</p>
+									    </h5>
+
+										<div class="icon">
+										<span class="time">10:00 AM</span>
+										<ul class="list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-up"> <i class="far fa-thumbs-up"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-down"><i class="far fa-thumbs-down"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-comments"><i class="far fa-comments"></i></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-chevron-down"><i class="fas fa-chevron-down"></i></a>
+								</li>
+							</ul>
+
+							<div class="buttonn">
+							<button class="button">js</button> 
+							<button class="button">js</button>
+							</div>
+
+										</div>
+
+									</div>
+								</div>
+								<div class="au-task__item au-task__item--warning">
+									<div class="au-task__item-inner">
+										<h5 class="task">
+											<a href="#">Create new task for Dashboard</a>
+										</h5>
+
+										<div class="icon">
+										<span class="time">11:00 AM</span>
+										<ul class="list-inline">
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-up"> <i class="far fa-thumbs-up"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-thumbs-down"><i class="far fa-thumbs-down"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-comments"><i class="far fa-comments"></i></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a href="#" class="btn-chevron-down"><i class="fas fa-chevron-down"></i></a>
+								</li>
+							</ul>
+										</div>
+
+										</div>
+
+									</div>
+								</div>
+							   
+							  
+							
+						
+						</div>
+					</div>
+				</div>
+
+			
+			</div>
+<!--news ahirlaxti-->
+
+<!--ders baxlandi-->
+			<div class="ders">
+			
+			<div class="col-lg-6 zzz">
+					<div class="au-card chart-percent-card">
+						<div class="au-card-inner">
+							<h3 class="title-2 tm-b-5">derıs  temisi </h3>
+							<div class="row no-gutters">
+								<div class="col-xl-6">
+									<div class="chart-note-wrap">
+										<div class="chart-note mr-0 d-block">
+											<span class="dot dot--blue"></span>
+											<span>products</span>
+										</div>
+										<div class="chart-note mr-0 d-block">
+											<span class="dot dot--red"></span>
+											<span>services</span>
+										</div>
+									</div>
+								</div>
+							   <div>ىتەكچى تور بىكىتىمىز ئابۇنىتلىرىمىز ئۈچۈن تۈرلۈك كومپىيۇتېر تىللىرىنى ئۈگۈتۈشنى مەقسەت قىلغان بۇلۇپ ئاددىي ئۇقۇم ۋە ئاددىي تىللار بىلەن ئىپادىلەپ ئۆگەنگۈچى ئەزالىرىمىزنىڭ ھەرقايسى تىللاردا بولغان سەۋىيسىنى ئوتتۇرھال سەۋيەدىن يۇقۇرى دەرىجىگە يەتكۈزۈشكە كاپالەت قىلىمىز، پەقەت سىزدە بىر دانە كومپىيۇتېر ۋە يىتەرلىك ۋاقىت بولسا بىزدە سىز ئىھتىياجلىق بولغان بىلىم مەنبەلىرى ۋە سۇئالىڭىزنىڭ جاۋابلىرى بىزدە بار، كومپىيۇتېر تېلىنى ئۆگۈنۈش پەقەت بىر تۇرمۇش ئۈچۈن بولماستىن ئۇ بىر ئارزۇ سىزنىڭ ئازۇيىڭىزنى ئەمەلگە ئاشۇرالايدىغان بىر بىلىمدۇر، بۇ تىلنى بىلگەن ئادەم مەيلى تۇرمۇشتا بولسۇن ياكى ئۆزى ئىرادە قىلغان ئىشلارنى ۋاستىلىك ھالدا ئەمەلگە ئاشۇرالايدۇ،. سىزنىڭ ئارزۇيىڭىز بىزنىڭ نىشانىمىز!</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+<!--ders ahirlaxti-->
+
+
+		</div>
+
+	</div>
+
+	
+ <!-- Jquery JS-->
+ <script src="../vendor/jquery-3.2.1.min.js"></script>
+  <!-- Bootstrap JS-->
+  <script src="../vendor/bootstrap-4.1/popper.min.js"></script>
+  <script src="../vendor/bootstrap-4.1/bootstrap.min.js"></script>
+  <!-- Vendor JS       -->
+  <script src="../vendor/slick/slick.min.js">
+  </script>
+  <script src="../vendor/wow/wow.min.js"></script>
+  <script src="../vendor/animsition/animsition.min.js"></script>
+  <script src="../vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+  </script>
+  <script src="../vendor/counter-up/jquery.waypoints.min.js"></script>
+  <script src="../vendor/counter-up/jquery.counterup.min.js">
+  </script>
+  <script src="../vendor/circle-progress/circle-progress.min.js"></script>
+  <script src="../vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="../vendor/chartjs/Chart.bundle.min.js"></script>
+  <script src="../vendor/select2/select2.min.js">
+  </script>
+
+  <!-- Main JS-->
+  <script src="../assist/js/main.js"></script>
+
+</body>
+</html>
